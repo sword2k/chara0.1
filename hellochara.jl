d260b142c352bf5c947f50a7013a0ce4b3dfaec2
sword2k <backtothedata@gmail.com>
2017-03-01 12:07:12 -0500

    added reply

--- hellochara.jl
+++ hellochara.jl
@@ -4,3 +4,12 @@ today = Dates.today()
 tomorrow = today + Dates.Day(1)
 tomorrowName = Dates.dayname(tomorrow)
 println(string("talk to me tomorrow, ",tomorrowName))
+
+
+tomorrowDay = Dates.dayofweek(tomorrow)
+tomorrowDay = 7
+if tomorrowDay < 5 || tomorrow == 7 
+   println(string(tomorrowName," is a weekday and I have work.."))
+elseif tomorrowDay == 6 || tomorrowDay == 5
+   println(string(tomorrowName," is a great day!"))
+end
